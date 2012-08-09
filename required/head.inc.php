084a11f0ab13bab971facee0b2cbd50aa2b50c23
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2012-08-09 22:41:22 +0100

    Fixed Facebook share button

--- required/head.inc.php
+++ required/head.inc.php
@@ -1,321 +1,321 @@
 <head>
-	<title>Norman Hartnell</title>
-	<meta name="description" content="The House of Hartnell, since 1923">
-	<meta name="keywords" content="sir, norman, hartnell, official, british, designer">
-	<meta name="author" content="Nostalgics & Savant">
-	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>Norman Hartnell</title>
+  <meta name="description" content="The House of Hartnell, since 1923">
+  <meta name="keywords" content="sir, norman, hartnell, official, british, designer">
+  <meta name="author" content="Nostalgics & Savant">
+  <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-	<meta property="og:image"              content="/images/logo-hartnell-flat.jpg">
-	<meta property="og:site_name"          content="Norman Hartnell">
-	<meta property="og:title"              content="Norman Hartnell">
-	<meta property="og:description"        content="Official website for Norman Hartnell" />
-	<meta property="og:type"               content="website">
-	<meta property="og:url"                content="http://normanhartnell.com">
-	<meta property="og:locale"             content="en_GB">
+  <meta property="og:image"       content="http://normanhartnell.com/images/slide-2.jpg">
+  <meta property="og:site_name"   content="Norman Hartnell">
+  <meta property="og:title"       content="Norman Hartnell">
+  <meta property="og:description" content="Official website for Norman Hartnell" />
+  <meta property="og:type"        content="website">
+  <meta property="og:url"         content="http://normanhartnell.com">
+  <meta property="og:locale"      content="en_GB">
 
-	<!-- Favicon Meta Tags (use both) -->
-	<link rel="icon"			href="/images/favicon.png"	type="image/png">
-	<link rel="shortcut icon"	href="/images/favicon.png"	type="image/png">
+  <!-- Favicon Meta Tags (use both) -->
+  <link rel="icon" href="/images/favicon.png" type="image/png">
+  <link rel="shortcut icon" href="/images/favicon.png" type="image/png">
 
-	<!--
-	/* @license
-	 * MyFonts Webfont Build ID 2156624, 2012-03-17T19:47:41-0400
-	 * 
-	 * The fonts listed in this notice are subject to the End User License
-	 * Agreement(s) entered into by the website owner. All other parties are 
-	 * explicitly restricted from using the Licensed Webfonts(s).
-	 * 
-	 * You may obtain a valid license at the URLs below.
-	 * 
-	 * Webfont: Proxima Nova A Bold Italic by Mark Simonson
-	 * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/a-bold-it/
-	 * Licensed pageviews: unspecified
-	 * 
-	 * Webfont: Proxima Nova S Bold Italic by Mark Simonson
-	 * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/s-bold-it/
-	 * Licensed pageviews: unspecified
-	 * 
-	 * Webfont: Proxima Nova S Bold by Mark Simonson
-	 * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/s-bold/
-	 * Licensed pageviews: unspecified
-	 * 
-	 * Webfont: Proxima Nova A Bold by Mark Simonson
-	 * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/a-bold/
-	 * Licensed pageviews: 10,000
-	 * 
-	 * Webfont: Proxima Nova Bold Italic by Mark Simonson
-	 * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/bold-it/
-	 * Licensed pageviews: unspecified
-	 * 
-	 * Webfont: Proxima Nova Bold by Mark Simonson
-	 * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/bold/
-	 * Licensed pageviews: unspecified
-	 * 
-	 * 
-	 * License: http://www.myfonts.com/viewlicense?type=web&buildid=2156624
-	 * Webfonts copyright: Copyright (c) Mark Simonson, 2005. All rights reserved.
-	 * 
-	 * © 2012 Bitstream Inc
-	 */
-	-->
+  <!--
+  /* @license
+   * MyFonts Webfont Build ID 2156624, 2012-03-17T19:47:41-0400
+   * 
+   * The fonts listed in this notice are subject to the End User License
+   * Agreement(s) entered into by the website owner. All other parties are 
+   * explicitly restricted from using the Licensed Webfonts(s).
+   * 
+   * You may obtain a valid license at the URLs below.
+   * 
+   * Webfont: Proxima Nova A Bold Italic by Mark Simonson
+   * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/a-bold-it/
+   * Licensed pageviews: unspecified
+   * 
+   * Webfont: Proxima Nova S Bold Italic by Mark Simonson
+   * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/s-bold-it/
+   * Licensed pageviews: unspecified
+   * 
+   * Webfont: Proxima Nova S Bold by Mark Simonson
+   * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/s-bold/
+   * Licensed pageviews: unspecified
+   * 
+   * Webfont: Proxima Nova A Bold by Mark Simonson
+   * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/a-bold/
+   * Licensed pageviews: 10,000
+   * 
+   * Webfont: Proxima Nova Bold Italic by Mark Simonson
+   * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/bold-it/
+   * Licensed pageviews: unspecified
+   * 
+   * Webfont: Proxima Nova Bold by Mark Simonson
+   * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/bold/
+   * Licensed pageviews: unspecified
+   * 
+   * 
+   * License: http://www.myfonts.com/viewlicense?type=web&buildid=2156624
+   * Webfonts copyright: Copyright (c) Mark Simonson, 2005. All rights reserved.
+   * 
+   * © 2012 Bitstream Inc
+   */
+  -->
 
-	<!--
-	/*
-	 * Emigre web font kit 4f4b8b1668f48
-	 * 
-	 * These fonts are not shareware. 
-	 * Use of these fonts requires the purchase 
-	 * of a license from Emigre, www.emigre.com  
-	 * Please contact sales@emigre.com
-	 *
-	 * To purchase a license to use this font go to:
-	 * http://www.emigre.com/fontpage.php?SMrR.html
-	 *
-	 * © 2011 Emigre, Inc
-	 */
-	-->
-	
-	<link rel="stylesheet" href="/stylesheets/styles.css" type="text/css">
+  <!--
+  /*
+   * Emigre web font kit 4f4b8b1668f48
+   * 
+   * These fonts are not shareware. 
+   * Use of these fonts requires the purchase 
+   * of a license from Emigre, www.emigre.com  
+   * Please contact sales@emigre.com
+   *
+   * To purchase a license to use this font go to:
+   * http://www.emigre.com/fontpage.php?SMrR.html
+   *
+   * © 2011 Emigre, Inc
+   */
+  -->
+  
+  <link rel="stylesheet" href="/stylesheets/styles.css" type="text/css">
 
-	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-	<script src="/javascript/plugins.js" type="text/javascript"></script>
-	<script src="/javascript/scripts.js" type="text/javascript"></script>
-	<script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
-	<script src="http://mint.normanhartnell.com/?js" type="text/javascript"></script>
+  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+  <script src="/javascript/plugins.js" type="text/javascript"></script>
+  <script src="/javascript/scripts.js" type="text/javascript"></script>
+  <script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
+  <script src="http://mint.normanhartnell.com/?js" type="text/javascript"></script>
 
-	<!-- Large Screens Media Query -->
-	<style type="text/css" media="only screen and (min-width:  1550px)">
-	body {font-size: 18px;line-height: 25px;}
+  <!-- Large Screens Media Query -->
+  <style type="text/css" media="only screen and (min-width:  1550px)">
+  body {font-size: 18px;line-height: 25px;}
 
-	/* Override jQuery smart resize */
-	img.auto-height {max-width: 745px;}
+  /* Override jQuery smart resize */
+  img.auto-height {max-width: 745px;}
 
-	.col1 {width: 30.0% !important;}
-	.col2 {width: 49.5% !important;}
-	.col3 {width: 66.0% !important;}
-	.col4 {width: 66.0% !important;}
-	.col5 {width: 82.5% !important;}
-	.col6 {width: 99.0% !important;}
-	
-	.col2 .col1 {width:  50% !important;}
-	.col2 .col2 {width: 100% !important;}
-	
-	.col3 .col1 {width: 33.4% !important;}
-	.col3 .col2 {width: 33.4% !important;}
-	.col3 .col3 {width:  100% !important;}
+  .col1 {width: 30.0% !important;}
+  .col2 {width: 49.5% !important;}
+  .col3 {width: 66.0% !important;}
+  .col4 {width: 66.0% !important;}
+  .col5 {width: 82.5% !important;}
+  .col6 {width: 99.0% !important;}
+  
+  .col2 .col1 {width:  50% !important;}
+  .col2 .col2 {width: 100% !important;}
+  
+  .col3 .col1 {width: 33.4% !important;}
+  .col3 .col2 {width: 33.4% !important;}
+  .col3 .col3 {width:  100% !important;}
 
-	.section > img {height: auto !important;}
-	#menswear .header-image img.fill {float:right; min-width: 100%; height: auto !important;}
-	#history .header-image img, #homeware .header-image img {width: auto !important;}
-	.section table#embroideries {zoom: 100%; width: 500px !important;}
-	</style>
+  .section > img {height: auto !important;}
+  #menswear .header-image img.fill {float:right; min-width: 100%; height: auto !important;}
+  #history .header-image img, #homeware .header-image img {width: auto !important;}
+  .section table#embroideries {zoom: 100%; width: 500px !important;}
+  </style>
 
-	<!-- Small Screens Media Query -->
-	<style type="text/css" media="only screen and (max-width:  1240px)">
-	/* move header to the top */
-	#header {
-		padding: 0;
-		width: 100%;
-		background-color: rgba(229,225,219,0.85);
-		box-shadow: none;
-		top: 0 !important;
-		z-index: 20;
-		-webkit-transition-property: background-color;
-		   -moz-transition-property: background-color;
-			 -o-transition-property: background-color;
-				transition-property: background-color;
-		-webkit-transition-duration: 1.2s;
-		   -moz-transition-duration: 1.2s;
-			 -o-transition-duration: 1.2s;
-				transition-duration: 1.2s;
-		}
-		#header > * {
-			width: auto;
-			display: block;
-			clear: both;
-			margin: 0 auto;
-			width: auto;
-			text-align: center;
-			}
-		#header img {
-			width: 125px;
-			padding: 15px 0;
-			float: none;
-			margin: 0 auto;
-			}
-		#header #nav {
-			}
-			#header #nav li {
-				display: inline-block;
-				margin: 0 10px 10px;
-				}
-
-
-
-	/* make the sections wider */
-	.section {
-		margin-left: 8.25%;
-		margin-top: 0;
-		margin-bottom: 0;
-		padding-top: 125px;
-		}
-
-	.header-image > img.fill {
-			float: right;
-			width: 100% !important;
-			height: auto !important;
-			}
-	
-	#slideshow {
-		width: 100% !important;
-		min-height: 790px;
-		margin-top: 120px;
-		margin-bottom: 70px;
-		margin-left: 0;
-		}
-		#slideshow img {
-			width: 100% !important;
-			height: auto !important;
-			}
-
-
-	.section {padding-bottom: 125px;}
-	.section > img {height: auto !important;}
-	.section.col2 {width: 100%;}
-
-	.section > img.col1, .section.col1 {width: 49.5% !important;}
-	.section > img.col2, .section.col2 {width: 66.0% !important;}
-	.section > img.col3, .section.col3 {width: 82.5% !important;}
-	.section > img.col4, .section.col4 {width: 82.5% !important;}
-	.section > img.col5, .section.col5 {width: 99.0% !important;}
-
-	.section > img.col1 {width:  50% !important;}
-	.section > img.col2 {width: 100% !important;}
-
-	.section > img.col1, .section .txt.col1 {width:   50% !important;}
-	.section > img.col2, .section .txt.col2 {width:   50% !important;}
-	.section > img.col3, .section .txt.col3 {width:  100% !important;}
-
-	.section table#embroideries {zoom: 60%;}
-
-
-	#footer {height: auto; margin: 140px 8.25% 50px; width: 82.5%; padding-left: 0;}
-		#footer > * {width: 50%;margin-bottom: 50px;}
-		#footer #share {width: 50%; position: static; float: right;}
-		#footer #credits {width: 50%; float: left;}
-		#footer h4#privacy-policy {padding-bottom: 0; margin-bottom: 0;}
-
-	@media only screen and (orientation: landscape) {
-		#slideshow {min-height: 1310px;}
-		.header-image img.fill {min-width: 100%; height: auto !important;}
-		#menswear .header-image img {width: 100% !important;}
-	}
-	@media only screen and (orientation: portrait) {
-		#slideshow {min-height: 960px;}
-		.section .txt.col3 {
-		-webkit-column-count: 2;
-		   -moz-column-count: 2;
-			 -o-column-count: 2;
-				column-count: 2;
-		}
-
-		#couture-and-wedding .txt.col3 p:nth-child(2) {padding-bottom: 0;}
-		.section .header-image, .section .header-image img {max-height: 470px;}
-	}
+  <!-- Small Screens Media Query -->
+  <style type="text/css" media="only screen and (max-width:  1240px)">
+  /* move header to the top */
+  #header {
+    padding: 0;
+    width: 100%;
+    background-color: rgba(229,225,219,0.85);
+    box-shadow: none;
+    top: 0 !important;
+    z-index: 20;
+    -webkit-transition-property: background-color;
+       -moz-transition-property: background-color;
+       -o-transition-property: background-color;
+        transition-property: background-color;
+    -webkit-transition-duration: 1.2s;
+       -moz-transition-duration: 1.2s;
+       -o-transition-duration: 1.2s;
+        transition-duration: 1.2s;
+    }
+    #header > * {
+      width: auto;
+      display: block;
+      clear: both;
+      margin: 0 auto;
+      width: auto;
+      text-align: center;
+      }
+    #header img {
+      width: 125px;
+      padding: 15px 0;
+      float: none;
+      margin: 0 auto;
+      }
+    #header #nav {
+      }
+      #header #nav li {
+        display: inline-block;
+        margin: 0 10px 10px;
+        }
 
 
 
-	</style>
+  /* make the sections wider */
+  .section {
+    margin-left: 8.25%;
+    margin-top: 0;
+    margin-bottom: 0;
+    padding-top: 125px;
+    }
 
-	<!-- iPhone -->
-	<style type="text/css" media="only screen and (max-width:  740px)">
+  .header-image > img.fill {
+      float: right;
+      width: 100% !important;
+      height: auto !important;
+      }
+  
+  #slideshow {
+    width: 100% !important;
+    min-height: 790px;
+    margin-top: 120px;
+    margin-bottom: 70px;
+    margin-left: 0;
+    }
+    #slideshow img {
+      width: 100% !important;
+      height: auto !important;
+      }
 
-	h1, h2         {font-size: 28px; line-height: 37px; letter-spacing: 5px;}
-	h3, h4, h5, h6 {font-size: 14px; line-height: 15px; letter-spacing:  2px;}
 
-	.section,
-	.col1,
-	.col2,
-	.col3,
-	.col4,
-	.col5,
-	.col6 {
-		width:  83.5% !important;
-		}
+  .section {padding-bottom: 125px;}
+  .section > img {height: auto !important;}
+  .section.col2 {width: 100%;}
 
-	#header,
-	.section .header-image,
-	.section > img,
-	.section > img.col1,
-	.section > img.col2,
-	.section > img.col1,
-	.section > img.col2,
-	.section > img.col3,
-	.section .txt,
-	.section .txt.col1,
-	.section .txt.col2,
-	.section .txt.col3 {
-		height: auto !important;
-		width:  100% !important;
-		-webkit-column-count: 1;
-		   -moz-column-count: 1;
-			 -o-column-count: 1;
-				column-count: 1;
-		}
+  .section > img.col1, .section.col1 {width: 49.5% !important;}
+  .section > img.col2, .section.col2 {width: 66.0% !important;}
+  .section > img.col3, .section.col3 {width: 82.5% !important;}
+  .section > img.col4, .section.col4 {width: 82.5% !important;}
+  .section > img.col5, .section.col5 {width: 99.0% !important;}
 
-	#header #nav {display: none;}
+  .section > img.col1 {width:  50% !important;}
+  .section > img.col2 {width: 100% !important;}
 
-	#slideshow {
-		margin-top: 95px;
-		min-height: 480px;
-		}
+  .section > img.col1, .section .txt.col1 {width:   50% !important;}
+  .section > img.col2, .section .txt.col2 {width:   50% !important;}
+  .section > img.col3, .section .txt.col3 {width:  100% !important;}
 
-	.header-image {
-		min-height: 300px;
-		width: auto !important;
-		}
-		.header-image > img {
-			min-height: 300px;
-			width: auto !important;
-			}
+  .section table#embroideries {zoom: 60%;}
 
-	.section > * {margin-bottom: 25px;}
 
-	.section table#embroideries {width: 260px !important;}
-	
-	.txt p {
-			margin: 0 0 30px !important;
-			padding: 0 !important;
-			}
+  #footer {height: auto; margin: 140px 8.25% 50px; width: 82.5%; padding-left: 0;}
+    #footer > * {width: 50%;margin-bottom: 50px;}
+    #footer #share {width: 50%; position: static; float: right;}
+    #footer #credits {width: 50%; float: left;}
+    #footer h4#privacy-policy {padding-bottom: 0; margin-bottom: 0;}
 
-	#footer {height: auto; margin-top: 0;}
-		#footer > * {width: 100%; clear: both;}
-		#footer #share {width: 100%;}
-		#footer #credits {width: 100%;}
-			#footer #credits > li {height: 30px;}
-		#footer #author {width: 100%; position: static; text-align: left;}
-		#footer h4#privacy-policy {padding-bottom: 10px;}
+  @media only screen and (orientation: landscape) {
+    #slideshow {min-height: 1310px;}
+    .header-image img.fill {min-width: 100%; height: auto !important;}
+    #menswear .header-image img {width: 100% !important;}
+  }
+  @media only screen and (orientation: portrait) {
+    #slideshow {min-height: 960px;}
+    .section .txt.col3 {
+    -webkit-column-count: 2;
+       -moz-column-count: 2;
+       -o-column-count: 2;
+        column-count: 2;
+    }
 
-	#newsletter input[type="email"]{
-		width: 200px;
-		}
-	@media all and (orientation: landscape) {
-		#slideshow {min-height: 620px;}
-		.header-image > img.fill {width:100% !important;}
-		#footer #credits > li {height: 15px;}
-	}
-	@media all and (orientation: portrait) {
-		#couture-and-wedding .header-image,
-		#menswear .header-image {
-			min-height: 210px !important;
-			}
-		#couture-and-wedding .header-image img.fill,
-		#menswear .header-image img.fill {
-			min-height: 0px !important;
-			}
+    #couture-and-wedding .txt.col3 p:nth-child(2) {padding-bottom: 0;}
+    .section .header-image, .section .header-image img {max-height: 470px;}
+  }
 
-	}
-	</style>
+
+
+  </style>
+
+  <!-- iPhone -->
+  <style type="text/css" media="only screen and (max-width:  740px)">
+
+  h1, h2         {font-size: 28px; line-height: 37px; letter-spacing: 5px;}
+  h3, h4, h5, h6 {font-size: 14px; line-height: 15px; letter-spacing:  2px;}
+
+  .section,
+  .col1,
+  .col2,
+  .col3,
+  .col4,
+  .col5,
+  .col6 {
+    width:  83.5% !important;
+    }
+
+  #header,
+  .section .header-image,
+  .section > img,
+  .section > img.col1,
+  .section > img.col2,
+  .section > img.col1,
+  .section > img.col2,
+  .section > img.col3,
+  .section .txt,
+  .section .txt.col1,
+  .section .txt.col2,
+  .section .txt.col3 {
+    height: auto !important;
+    width:  100% !important;
+    -webkit-column-count: 1;
+       -moz-column-count: 1;
+       -o-column-count: 1;
+        column-count: 1;
+    }
+
+  #header #nav {display: none;}
+
+  #slideshow {
+    margin-top: 95px;
+    min-height: 480px;
+    }
+
+  .header-image {
+    min-height: 300px;
+    width: auto !important;
+    }
+    .header-image > img {
+      min-height: 300px;
+      width: auto !important;
+      }
+
+  .section > * {margin-bottom: 25px;}
+
+  .section table#embroideries {width: 260px !important;}
+  
+  .txt p {
+      margin: 0 0 30px !important;
+      padding: 0 !important;
+      }
+
+  #footer {height: auto; margin-top: 0;}
+    #footer > * {width: 100%; clear: both;}
+    #footer #share {width: 100%;}
+    #footer #credits {width: 100%;}
+      #footer #credits > li {height: 30px;}
+    #footer #author {width: 100%; position: static; text-align: left;}
+    #footer h4#privacy-policy {padding-bottom: 10px;}
+
+  #newsletter input[type="email"]{
+    width: 200px;
+    }
+  @media all and (orientation: landscape) {
+    #slideshow {min-height: 620px;}
+    .header-image > img.fill {width:100% !important;}
+    #footer #credits > li {height: 15px;}
+  }
+  @media all and (orientation: portrait) {
+    #couture-and-wedding .header-image,
+    #menswear .header-image {
+      min-height: 210px !important;
+      }
+    #couture-and-wedding .header-image img.fill,
+    #menswear .header-image img.fill {
+      min-height: 0px !important;
+      }
+
+  }
+  </style>
 
 </head>
