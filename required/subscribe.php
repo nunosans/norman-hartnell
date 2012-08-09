da33f8cd0958d81253e0ff14f3ac0fe0ece268de
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2012-08-09 15:08:58 +0100

    Added Danielle Sheridan’s email address as notifications recipient

--- required/subscribe.php
+++ required/subscribe.php
@@ -1,6 +1,6 @@
 <?php
-$to = "info@normanhartnell.com";
-$subject = "New subscriber";
+$to = "daniellesheridan@theglintproject.com";
+$subject = "Norman Hartnell: New Subscriber";
 $email = $_REQUEST['email'];
 $message = "A new email address has been submitted to your newsletter signup box: $email";
 $headers = "From: notifications@normanhartnell.com";
